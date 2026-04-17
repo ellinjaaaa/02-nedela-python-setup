d70c83d5861249f97a070c62ecea06a34541e1c4
Ella Luize Zarane <Ella.Luize.Zarane@fita.lv>
2026-04-17 23:09:47 +0300

    fix: added recognition of player's win/loss

--- guess.py
+++ guess.py
@@ -6,6 +6,8 @@ print("Tev ir 10 mēģinājumi, lai uzminētu skaitli!")
 
 number_of_guesses=0
 
+player_win=False #sākumā nosaku, ka spēlētājs vēl nav vinnējis - =False
+
 while number_of_guesses<10:
     izvele=int(input("Izvēlies skaitli no 1 līdz 100:"))
     number_of_guesses+=1 #palielina mēģinājumu skaitu par 1
@@ -15,8 +17,10 @@ while number_of_guesses<10:
     elif izvele<a:
         print("Tevis izvēlētais skaitlis ir par mazu!")
     else:
+        player_win=True #ja spēlētājs uzmin, tad tagad spēlētājs ir vinnējis, tādēļ =True
         print("Apsveicu! Tu uzminēji skaitli!")
         break
 
-print(f"Pareizas skaitlis: {a}")
-exit()
+if player_win==False: #ja 10 mēģinājumi ir beigušies, bet spēlētājs nav joprojām uzminējis, tad izvada pareizo skaitli
+    print(f"Pareizas skaitlis: {a}")
+    exit()
